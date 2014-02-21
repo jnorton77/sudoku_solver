626f32900e1a191f3d3a10007fa61869378cb3d7
John_and_Tony <tonyta.tt@gmail.com>
2014-02-21 13:51:29 -0600

    adds tests for all cols

--- rows_and_cols.rb
+++ rows_and_cols.rb
@@ -35,17 +35,18 @@ row_board =
     8, 8, 8, 8, 8, 8, 8, 8, 8,
     9, 9, 9, 9, 9, 9, 9, 9, 9 ]
 
-col_1_board =
-  [ 1, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0, 0 ]
+col_board =
+  [ 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 
+puts "Row Tests"
 puts get_row(1, row_board) == Array.new(9, 1)
 puts get_row(2, row_board) == Array.new(9, 2)
 puts get_row(3, row_board) == Array.new(9, 3)
@@ -55,5 +56,14 @@ puts get_row(6, row_board) == Array.new(9, 6)
 puts get_row(7, row_board) == Array.new(9, 7)
 puts get_row(8, row_board) == Array.new(9, 8)
 puts get_row(9, row_board) == Array.new(9, 9)
-
-# puts get_col(1, col_1_board) == solution
+puts
+puts "Col Tests"
+puts get_col(1, col_board) == Array.new(9, 1)
+puts get_col(2, col_board) == Array.new(9, 2)
+puts get_col(3, col_board) == Array.new(9, 3)
+puts get_col(4, col_board) == Array.new(9, 4)
+puts get_col(5, col_board) == Array.new(9, 5)
+puts get_col(6, col_board) == Array.new(9, 6)
+puts get_col(7, col_board) == Array.new(9, 7)
+puts get_col(8, col_board) == Array.new(9, 8)
+puts get_col(9, col_board) == Array.new(9, 9)
