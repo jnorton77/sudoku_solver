16915fbac72cec6d091e9ecf8d58cb336d825963
John_and_Tony <tonyta.tt@gmail.com>
2014-02-21 13:39:36 -0600

    implements get_row method

--- rows_and_cols.rb
+++ rows_and_cols.rb
@@ -1,9 +1,17 @@
 
 def get_row(row_num, board)
+  n = 9
+  index = row_num - 1
+  array = []
+  1.upto(9){|idx| array << board[idx * n * (row_num-1)] }
+  array
 end
 
-def get_col(col_num, board)
-end
+# def get_col(col_num, board)
+#   n = 9
+#   index = col_num - 1
+
+# end
 
 # TESTS
 solution = [ 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
@@ -42,4 +50,5 @@ col_1_board =
     1, 0, 0, 0, 0, 0, 0, 0, 0 ]
 
 puts get_row(1, row_1_board) == solution
-puts get_col(1, col_1_board) == solution
+# puts get_row(1, row_1_board)
+# puts get_col(1, col_1_board) == solution
