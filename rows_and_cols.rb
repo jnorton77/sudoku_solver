a97f6dba5c9cd5c1fe096555e6c701a0b601a150
John_and_Tony <tonyta.tt@gmail.com>
2014-02-21 13:48:56 -0600

    debugs get_row and adds more row tests

--- rows_and_cols.rb
+++ rows_and_cols.rb
@@ -2,16 +2,13 @@
 def get_row(row_num, board)
   n = 9
   index = row_num - 1
-  array = []
-  1.upto(9){|idx| array << board[idx * n * (row_num-1)] }
-  array
+  board.each_slice(9).to_a[index]
 end
 
-# def get_col(col_num, board)
-#   n = 9
-#   index = col_num - 1
-
-# end
+def get_col(col_num, board)
+  # new_board = board.each_slice(9).to_a.transpose.flatten
+  # get_row(col_num, board)
+end
 
 # TESTS
 solution = [ 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
@@ -27,16 +24,16 @@ empty_board =
     0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0 ]
 
-row_1_board =
+row_board =
   [ 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    3, 3, 3, 3, 3, 3, 3, 3, 3,
+    4, 4, 4, 4, 4, 4, 4, 4, 4,
+    5, 5, 5, 5, 5, 5, 5, 5, 5,
+    6, 6, 6, 6, 6, 6, 6, 6, 6,
+    7, 7, 7, 7, 7, 7, 7, 7, 7,
+    8, 8, 8, 8, 8, 8, 8, 8, 8,
+    9, 9, 9, 9, 9, 9, 9, 9, 9 ]
 
 col_1_board =
   [ 1, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -49,6 +46,14 @@ col_1_board =
     1, 0, 0, 0, 0, 0, 0, 0, 0,
     1, 0, 0, 0, 0, 0, 0, 0, 0 ]
 
-puts get_row(1, row_1_board) == solution
-# puts get_row(1, row_1_board)
+puts get_row(1, row_board) == Array.new(9, 1)
+puts get_row(2, row_board) == Array.new(9, 2)
+puts get_row(3, row_board) == Array.new(9, 3)
+puts get_row(4, row_board) == Array.new(9, 4)
+puts get_row(5, row_board) == Array.new(9, 5)
+puts get_row(6, row_board) == Array.new(9, 6)
+puts get_row(7, row_board) == Array.new(9, 7)
+puts get_row(8, row_board) == Array.new(9, 8)
+puts get_row(9, row_board) == Array.new(9, 9)
+
 # puts get_col(1, col_1_board) == solution
