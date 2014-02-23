458efe657097695682cce7a942cc6cc901bd5609
Tony Ta <tonyta.tt@gmail.com>
2014-02-23 16:44:10 -0600

    encapsulates solve logic into another file and module

---
+++ board_logic.rb
@@ -0,0 +1,37 @@
+module SudokuBoardLogic
+  def brute_squad
+    brute_board = board_array
+    first_array_index = brute_board.index{ |index| index.is_a?(Array) && index.length == 2 }
+    guess_1 = brute_board[first_array_index].shift
+    guess_2 = brute_board[first_array_index].pop
+    brute_board[first_array_index] = guess_1
+    guess_seed = brute_board.map {|element| element.is_a?(Array) ? element = 0 : element}
+    solution = self.class.new(guess_seed.join).solve
+    if solution
+      self.board_array = create_board( solution )
+    else
+      puts "You are the Brute Squad!"
+      brute_board[first_array_index] = guess_2
+      guess_seed = brute_board.map {|element| element.is_a?(Array) ? element = 0 : element}
+      solution = self.class.new(guess_seed.join).solve
+    end
+  end
+
+  def solve_by_elimination
+    board_array.each_index do |index|
+      update_cell(index)
+    end
+  end
+
+  def update_cell(index)
+    if board_array[index].is_a? Array
+      self.board_array[index] = board_array[index] - get_row(get_row_num(index))
+      self.board_array[index] = board_array[index] - get_col(get_col_num(index))
+      self.board_array[index] = board_array[index] - get_grid(get_grid_num(index))
+      if board_array[index].length == 1
+        self.board_array[index] = board_array[index].pop
+      end
+    end
+  end
+
+end
--- sudoku.rb
+++ sudoku.rb
@@ -1,6 +1,8 @@
 class SudokuBoard
   require './board_structure'
+  require './board_logic'
   include SudokuBoardStructure
+  include SudokuBoardLogic
 
   attr_accessor :array_of_cells, :board_array, :indices_of_grids_array, :last_array, :board_string
 
@@ -35,47 +37,12 @@ class SudokuBoard
     board_array.join
   end
 
-  def brute_squad
-    brute_board = board_array
-    first_array_index = brute_board.index{ |index| index.is_a?(Array) && index.length == 2 }
-    guess_1 = brute_board[first_array_index].shift
-    guess_2 = brute_board[first_array_index].pop
-    brute_board[first_array_index] = guess_1
-    guess_seed = brute_board.map {|element| element.is_a?(Array) ? element = 0 : element}
-    solution = self.class.new(guess_seed.join).solve
-    if solution
-      self.board_array = create_board( solution )
-    else
-      puts "You are the Brute Squad!"
-      brute_board[first_array_index] = guess_2
-      guess_seed = brute_board.map {|element| element.is_a?(Array) ? element = 0 : element}
-      solution = self.class.new(guess_seed.join).solve
-    end
-  end
-
   def need_guess?
     if board_array.flatten == last_array
       true
     else
       self.last_array = board_array.flatten
       false
-    end
-  end
-
-  def solve_by_elimination
-    board_array.each_index do |index|
-      update_cell(index)
-    end
-  end
-
-  def update_cell(index)
-    if board_array[index].is_a? Array
-      self.board_array[index] = board_array[index] - get_row(get_row_num(index))
-      self.board_array[index] = board_array[index] - get_col(get_col_num(index))
-      self.board_array[index] = board_array[index] - get_grid(get_grid_num(index))
-      if board_array[index].length == 1
-        self.board_array[index] = board_array[index].pop
-      end
     end
   end
 
