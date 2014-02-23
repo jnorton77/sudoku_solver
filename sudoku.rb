a0c4ea60a9f68b38d543c062aa6b4bb68e6492c8
Tony Ta <tonyta.tt@gmail.com>
2014-02-23 16:34:49 -0600

    encapsulates driver code into init.rb

--- sudoku.rb
+++ sudoku.rb
@@ -121,28 +121,3 @@ class SudokuBoard
 
   alias_method :to_s, :print_board
 end
-
-# # # ### ORIGINAL DRIVER CODE ###
-
-unsolved_problems = File.readlines('problem_sets/dbc_sudoku.txt').map(&:chomp)
-unsolved_problems.each_with_index do |board_string, index|
-  puts "Problem number #{index+1}"
-  puts board_string
-  puts
-  game = SudokuBoard.new(board_string)
-  puts "Before:"
-  game.print_board
-  puts
-  puts "After:"
-  game.solve!
-  puts
-  puts
-end
-
-# test = SudokuBoard.new("8000000000036000000700902000500070000000100030001000068008500010090000400")
-# puts board_string
-# puts "Before:"
-# test.print_board
-# puts
-# puts "After:"
-# test.solve!
